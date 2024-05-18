d1bcd19ef8c56e4dc70d9e8be6c72ef8f5892d75
kosmogon <74239768+kosmogon@users.noreply.github.com>
2024-05-18 15:05:56 +0200

    Create plan.py

---
+++ src /plan.py
@@ -0,0 +1,23 @@
+#Models to be used and discussed in the report:
+#- Linear Regression: multiple & ridge
+#- XGBoost: with parameter optimization algorithms (grid search, Bayessian) and without (interpretable: low number of trees, low depth)
+#- LSTM (important for the field of wind power forecasting)
+#- EBM / GAM
+
+
+#To-Dos:
+#- Preprocessing: exclude observations where: Y = 0 & Wind Speed > Cut-In speed
+#- Preproccesing: explain negative power values
+#- XGBoost: figure out what a leaf value represents
+
+
+#Web-Application: 
+#- Pages for different models
+#- Possibility to make predictions for different turbines separately and for the whole wind farm (all the turbines combined)
+#- Include different accuracy metrics in the output (MAE, R2, etc.)
+
+
+
+
+#Questions for Charlie:
+#Where might negative power values come from? (~9% of the rows)
