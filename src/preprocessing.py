2a7cb57b3aeeefb0420ae2a9c6b1d499ec7824b7
Nikolas Witt <nwitt.lpz@gmail.com>
2024-05-23 10:29:24 +0200

    replace wind direction with categorical values

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -17,6 +17,26 @@ def combine_values_hourly(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def encode_wind_direction(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Encode the wind direction column to numerical values using one-hot encoding
+    """
+
+    # encode the wind direction column, drop the first column to avoid multicollinearity
+    encoder = OneHotEncoder(handle_unknown="ignore", sparse_output=False, drop='first').set_output(transform="pandas")
+
+    # fit and transform the column
+    encoded_columns = encoder.fit_transform(df[['Wind direction (°)']])
+
+    # name the new columns by the cell values without 'Wind direction' in front
+    encoded_columns.columns = [col.split('Wind direction (°)_')[1].strip() for col in encoded_columns.columns]
+
+    # drop the original column and concatenate the encoded columns
+    df = pd.concat([df, encoded_columns], axis=1).drop(columns=['Wind direction (°)'])
+
+    return df
+
+
 def handle_missing_values(df: pd.DataFrame) -> pd.DataFrame:
     """
     Handle missing values in the dataframe
