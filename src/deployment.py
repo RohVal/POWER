b2b149664c0cf30130eebfc9900e3d5f7b21473a
Rohit V <rv19waza@studserv.uni-leipzig.de>
2024-06-06 01:03:22 +0200

    wind direction implementation for  deployment/interface

--- src/deployment.py
+++ src/deployment.py
@@ -5,26 +5,33 @@ import pandas as pd
 model = load_model("xgboost.pkl")
 
 CONTINUOUS_FEATURES = ["Wind speed (m/s)", "Wind speed - Maximum (m/s)", "Wind speed - Minimum (m/s)",
-                       "Nacelle ambient temperature (°C)", "Wind direction (°)"]
+                       "Nacelle ambient temperature (°C)"]
+
+WIND_DIRECTION_FEATURES = ["E/NE", "E/SE", "N", "N/NE", "N/NW", "NE", "NW", "S", "S/SE", "S/SW", "SE", "SW", "W", "W/NW", "W/SW"]
 
 PREDICTION_NAME = "Power (kW)"
+
 LOGICAL_BOUNDS = {
     "Wind speed (m/s)": (0, 27),
     "Wind speed - Maximum (m/s)": (0, 27),
     "Wind speed - Minimum (m/s)": (0, 27),
-    "Nacelle ambient temperature (°C)": (-20, 42),
-    "Wind direction (°)": (0, 360)
+    "Nacelle ambient temperature (°C)": (-20, 42)
 }
-
 
 def predict_power(wind_speed, wind_speed_max, wind_speed_min, nacelle_temp, wind_direction):
     data = {
-        "Wind direction (°)": wind_direction,
         "Wind speed (m/s)": wind_speed,
         "Wind speed - Maximum (m/s)": wind_speed_max,
         "Wind speed - Minimum (m/s)": wind_speed_min,
         "Nacelle ambient temperature (°C)": nacelle_temp,
     }
+
+    # setting the value for all directions in the matrix to 0
+    for direction in WIND_DIRECTION_FEATURES:
+        data[direction] = 0
+
+    # Changing the value of the selected direction to 1
+    data[wind_direction] = 1
 
     for feature in CONTINUOUS_FEATURES:
         if data[feature] < LOGICAL_BOUNDS[feature][0] or data[feature] > LOGICAL_BOUNDS[feature][1]:
@@ -34,9 +41,29 @@ def predict_power(wind_speed, wind_speed_max, wind_speed_min, nacelle_temp, wind
     prediction = model.predict(df)[0]
     return prediction
 
-
 # Create the interface
-inputs = [gr.Slider(minimum=LOGICAL_BOUNDS[feature][0], maximum=LOGICAL_BOUNDS[feature]
-                    [1], label=feature) for feature in CONTINUOUS_FEATURES]
+inputs = [
+    gr.Slider(minimum=LOGICAL_BOUNDS[feature][0], maximum=LOGICAL_BOUNDS[feature][1], label=feature)
+    for feature in CONTINUOUS_FEATURES
+]
+
+# Directions in dropdown list
+inputs.append(gr.Dropdown(choices=WIND_DIRECTION_FEATURES, label="Wind direction"))
+
+## Directions in radio buttons format
+# inputs.append(gr.Radio(choices=WIND_DIRECTION_FEATURES, label="Wind direction"))
+
 outputs = gr.Textbox(label=PREDICTION_NAME)
-gr.Interface(fn=predict_power, inputs=inputs, outputs=outputs).launch()
+
+## Some example Inputs to check the 
+examples = [
+    [7.26, 9.6, 5.28, 12.56, 'S'],
+    [7.92, 9.54, 5.53, 13.19, 'S/SW'],
+    [12, 15, 10, 25, 'N'],
+    [8, 10, 6, 18, 'E/NE'],
+    [20, 22, 18, 30, 'S'],
+    [5, 7, 4, 10, 'W'],
+]
+
+# Launch the interface
+gr.Interface(fn=predict_power, inputs=inputs, outputs=outputs, examples=examples).launch()
