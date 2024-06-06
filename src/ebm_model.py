256e7b9e3c4cda406f7a308efb3f002b76444059
Rohit V <rv19waza@studserv.uni-leipzig.de>
2024-06-06 17:54:39 +0200

    ebm model path fixed

--- src/ebm_model.py
+++ src/ebm_model.py
@@ -11,7 +11,7 @@ from utils import save_model, split_data
 from interpret.glassbox import ExplainableBoostingClassifier
 from interpret import show
 
-df = preprocess_data(path='/content/drive/MyDrive/BI /first prototype/Turbine_Data_Kelmarsh_1_clean.csv')
+df = preprocess_data(path="../dataset/Turbine_Data_Kelmarsh_1_2022-01-01_-_2023-01-01_228.csv")
 df = df.drop(columns=['Date and time'])
 
 X_train, X_test, y_train, y_test = split_data(df=df)
