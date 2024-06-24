1792790d7d83a328481dc82a10a21dd8c5b5994c
Rohit V <rv19waza@studserv.uni-leipzig.de>
2024-06-24 18:19:17 +0200

    webapp prediction updates

--- TODO.md
+++ TODO.md
@@ -1,11 +1,11 @@
-[] Feature Fields with slider for wind speed, max, min and temperature.
+[x] Feature Fields with slider for wind speed, max, min and temperature.
 [] The user inputs windspeed(incl. max and min) in km/h only for frontend
-[] For wind direction: dropdown or radio buttons
+[x] For wind direction: dropdown or radio buttons
 [] Fixed input fields that output a tabular result for all models.
 [] Graphs for explainable models 
 [] Predict button: to make the prediction(on the backend)
 [] Output: in kWh
 [] something that tells the user that the power generated is for one hour 
 [] Accuracy: 
-[] Weather api gives insight for the last 7 days. We use this data to make a weekly dashboard.
+[x] Weather api gives insight for the last 7 days. We use this data to make a weekly dashboard.
 [] Talk to EBM/ Chatbot solution. 
--- WebApp/app.py
+++ WebApp/app.py
@@ -1,23 +1,24 @@
 import pandas as pd
-import pickle
+from pickle import load
 from flask import Flask, request, jsonify, render_template
+from interpret import show, preserve
 
 app = Flask(__name__)
 
-## Using the same parameters and values as the gradio model 
-# # Load models
-# with open(r"C:\Users\rexxt\POWER\models\ebm_model.pkl", "rb") as f: 
-#     model_grid = pickle.load(f)
-# with open(r"C:\Users\rexxt\POWER\models\ebm_model.pkl", "rb") as f:
-#     model_xgboost = pickle.load(f)
-# with open(r"C:\Users\rexxt\POWER\models\ebm_model.pkl", "rb") as f:
-#     model_ebm = pickle.load(f)
+# Using the same parameters and values as the gradio model 
+# Load models
+with open(r"/models/ebm_model.pkl", "rb") as f: 
+    model_grid = load(f)
+with open(r"/models/ebm_model.pkl", "rb") as f:
+    model_xgboost = load(f)
+with open(r"/models/ebm_model.pkl", "rb") as f:
+    model_ebm = load(f)
 
 # Constants
 CONTINUOUS_FEATURES = ["Wind speed (m/s)", "Wind speed - Maximum (m/s)", "Wind speed - Minimum (m/s)",
                        "Nacelle ambient temperature (°C)"]
 
-WIND_DIRECTION_FEATURES = ["E/NE", "E/SE", "N", "N/NE", "N/NW", "NE", "NW", "S", "S/SE", "S/SW", "SE", "SW", "W", "W/NW", "W/SW"]
+WIND_DIRECTION_FEATURES = ["E", "E/NE", "E/SE", "N", "N/NE", "N/NW", "NE", "NW", "S", "S/SE", "S/SW", "SE", "SW", "W", "W/NW", "W/SW"]
 
 PREDICTION_NAME = "Power (kW)(generated in one hour)"
 LOGICAL_BOUNDS = {
@@ -45,26 +46,37 @@ def predict_power(model, wind_speed, wind_speed_max, wind_speed_min, nacelle_tem
 
     df = pd.DataFrame([data])
     prediction = model.predict(df)[0]
+    # preserve(model.explain_global(), file_name = "xyz.html")
+    # preserve(show(model.explain_global()), file_name = "shobc")
     return prediction
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict')
+@app.route('/predict', methods = ['GET','POST'])
 def predict():
-    return render_template('predict.html')
-    # if model_type == 'Grid':
-    #     model = model_grid
-    # elif model_type == 'XGBoost':
-    #     model = model_xgboost
-    # elif model_type == 'EBM':
-    #     model = model_ebm
-    # else:
-    #     return jsonify({"error": "Invalid model type"}), 400
 
-    # prediction = predict_power(model, wind_speed, wind_speed_max, wind_speed_min, nacelle_temp, wind_direction)
-    # return jsonify({"prediction": prediction})
+    model = model_ebm
+    if request.method == "POST":
+        model_type = request.form.get("model")
+        if model_type == 'XGBoost':
+            model = model_xgboost
+        elif model_type == 'EBM':
+            model = model_ebm
+        else:
+            return jsonify({"error": "Invalid model type"}), 400
+            
+        wind_speed = int(request.form.get("wspeed"))
+        wind_speed_max = int(request.form.get("wspeed_max"))
+        wind_speed_min = int(request.form.get("wspeed_min"))
+        nacelle_temp = int(request.form.get("ntemp"))
+        wind_direction = (request.form.get("wind_direction"))
+
+        prediction = predict_power(model, wind_speed, wind_speed_max, wind_speed_min, nacelle_temp, wind_direction)
+
+        return render_template('predict.html', result = prediction )
+    return render_template('predict.html')    
 
 if __name__ == '__main__':
     app.run(debug=True)
